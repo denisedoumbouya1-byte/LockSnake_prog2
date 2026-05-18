82357025942b72978565ef6ce6b3828ceb95df29
denisedoumbouya1-byte <denisedoumbouya1@gmail.com>
2026-05-18 03:05:50 +0200

    initial commit

---
+++ src/main/java/de/hsbi/lockgame/logic/GameEngine.java
@@ -0,0 +1,54 @@
+package de.hsbi.lockgame.logic;
+
+import de.hsbi.lockgame.model.Direction;
+import de.hsbi.lockgame.model.Level;
+import de.hsbi.lockgame.model.Snake;
+import de.hsbi.lockgame.ui.GamePanel;
+
+import java.util.List;
+
+public final class GameEngine {
+
+    private GameState state;
+    private GamePanel panel;
+
+    public GameEngine(Level level) {
+        this.state =
+            new GameState(
+                level,
+                new Snake(List.of(level.snakeStart())),
+                level.pins(),
+                GameState.Status.RUNNING,
+                Direction.NONE);
+    }
+
+    public GameState state() {
+        return state;
+    }
+
+    public void setGamePanel(GamePanel panel) {
+        this.panel = panel;
+    }
+
+    public void update(Direction d) {
+        this.state =
+            new GameState(
+                state.level(),
+                state.snake(),
+                state.pins(),
+                state.status(),
+                d);
+
+        if (panel != null) {
+            panel.update(state);
+        }
+    }
+
+    public void tick() {
+        state = state.tick();
+
+        if (panel != null) {
+            panel.update(state);
+        }
+    }
+}
